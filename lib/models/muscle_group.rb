0df64b5768b7e4c741c55a67faef8b16fc8a1a12
NaveedG <nganjani@protonmail.com>
2019-10-24 10:54:22 -0500

    fixed delete methods and added new method

--- lib/models/muscle_group.rb
+++ lib/models/muscle_group.rb
@@ -2,12 +2,12 @@ class MuscleGroup < ActiveRecord::Base
   has_many :exercises
   has_many :equipments, through: :exercises
 
-  def add_muscle_group(name)
-    MuscleGroup.create(name)
+  def self.add_muscle_group(name)
+    self.create(name)
   end
 
-  def delete_muscle_group(name)
-    MuscleGroup.delete_by(name: name)
+  def self.delete_muscle_group(name)
+    self.find_by_name(name).destroy
   end
 
   def self.display_all_names
@@ -15,7 +15,7 @@ class MuscleGroup < ActiveRecord::Base
   end
 
   def self.find_by_name(name)
-    self.find_by name: name
+    self.find_by(name: name)
   end
 
 end
